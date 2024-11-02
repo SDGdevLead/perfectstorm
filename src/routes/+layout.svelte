d5ba601cbb27987e8e235aeac10536efda75ca0d
Brandon <brandon.ohboil@gmail.com>
2024-11-02 00:13:33 -0400

    perf upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import "../app.postcss";
+  import Navbar from "$lib/components/Navbar.svelte";
+
+  // Floating UI for Popups
+  import {
+    computePosition,
+    autoUpdate,
+    flip,
+    shift,
+    offset,
+    arrow,
+  } from "@floating-ui/dom";
+  import { storePopup } from "@skeletonlabs/skeleton";
+  storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<div class="relative p-24">
+  <Navbar />
+
+  <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,198 @@
+<script>
+</script>
+
+<head> </head>
+
+<div
+  class="bg-surface-800 p-8 flex flex-col items-center justify-center text-6xl opacity-85"
+>
+  <p class="gradient-heading">Seven Golden Letters</p>
+  <p class="gradient-slice">One Weekend</p>
+</div>
+
+<!-- Gallery -->
+<div
+  class="relative flex items-center justify-center bg-surface-800 opacity-85 rounded-container-token mt-8"
+>
+  <div class="p-8"></div>
+
+  <div
+    class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-5 p-8 drop-shadow-2xl"
+  >
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+    <div>
+      <img
+        class="object-cover object-center w-full h-40 max-w-full rounded-lg drop-shadow-2xl"
+        src=""
+        alt=""
+      />
+    </div>
+  </div>
+</div>
